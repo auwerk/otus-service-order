fca429fe2e1ac49f21c7176a80edae0b18ed596c
auwerk <noisefront@yandex.ru>
2023-03-16 21:38:32 +0300

    Order domain data class

---
+++ src/main/java/org/auwerk/otus/arch/orderservice/domain/Order.java
@@ -0,0 +1,17 @@
+package org.auwerk.otus.arch.orderservice.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Order {
+    private UUID id;
+    private String productCode;
+    private Integer quantity;
+    private LocalDateTime createdAt;
+    private LocalDateTime placedAt;
+}
