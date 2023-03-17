845aefa7422ab9fb91d7a3a0bd0b1917f7ad4e3e
auwerk <noisefront@yandex.ru>
2023-03-17 18:41:18 +0300

    Placement API enhancements, error handling

--- src/main/java/org/auwerk/otus/arch/orderservice/api/OrderResource.java
+++ src/main/java/org/auwerk/otus/arch/orderservice/api/OrderResource.java
@@ -1,15 +1,21 @@
 package org.auwerk.otus.arch.orderservice.api;
 
+import java.util.UUID;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.auwerk.otus.arch.orderservice.api.dto.CreateOrderResponseDto;
 import org.auwerk.otus.arch.orderservice.api.dto.PlaceOrderRequestDto;
+import org.auwerk.otus.arch.orderservice.exception.OrderAlreadyPlacedException;
+import org.auwerk.otus.arch.orderservice.exception.OrderNotFoundException;
 import org.auwerk.otus.arch.orderservice.mapper.OrderMapper;
 import org.auwerk.otus.arch.orderservice.service.OrderService;
 
@@ -34,9 +40,15 @@ public class OrderResource {
     }
 
     @PUT
-    public Uni<Response> placeOrder(PlaceOrderRequestDto requestDto) {
+    @Path("/{orderId}")
+    public Uni<Response> placeOrder(@PathParam("orderId") UUID orderId, PlaceOrderRequestDto requestDto) {
         return orderService
-                .placeOrder(requestDto.getOrderId(), requestDto.getProductCode(), requestDto.getQuantity())
-                .map(placedOrder -> Response.ok(orderMapper.toDto(placedOrder)).build());
+                .placeOrder(orderId, requestDto.getProductCode(), requestDto.getQuantity())
+                .onItem()
+                .transform(placedOrder -> Response.ok(orderMapper.toDto(placedOrder)).build())
+                .onFailure(OrderNotFoundException.class)
+                .recoverWithItem(Response.status(Status.NOT_FOUND).build())
+                .onFailure(OrderAlreadyPlacedException.class)
+                .recoverWithItem(Response.status(Status.CONFLICT).build());
     }
 }
--- src/main/java/org/auwerk/otus/arch/orderservice/api/dto/PlaceOrderRequestDto.java
+++ src/main/java/org/auwerk/otus/arch/orderservice/api/dto/PlaceOrderRequestDto.java
@@ -1,14 +1,11 @@
 package org.auwerk.otus.arch.orderservice.api.dto;
 
-import java.util.UUID;
-
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 @Data
 @RegisterForReflection
 public class PlaceOrderRequestDto {
-    private UUID orderId;
     private String productCode;
     private Integer quantity;
 }
